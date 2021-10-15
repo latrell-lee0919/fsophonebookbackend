59bc93bfd82f3375e06b78faa538648bc903db0c
Latrell <lll4xy@virginia.edu>
2021-10-15 12:52:30 -0400

    heroku deployment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-app = express()
+const cors = require('cors')
 
+app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan.token('data', (req, res) => { 
     const data = {"name": req.body.name, "number": req.body.number};
@@ -111,7 +113,7 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
