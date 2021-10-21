6a31ac791fc016ffb32600e3df1cb1778e389238
Latrell <lll4xy@virginia.edu>
2021-10-21 17:31:04 -0400

    added error handling

--- index.js
+++ index.js
@@ -139,6 +139,20 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+  
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
