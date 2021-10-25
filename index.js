1e8e2c190f1065f3a469b6cec25596c9786d6d3d
Latrell <lll4xy@virginia.edu>
2021-10-25 16:42:12 -0400

    exercise 3.22

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-unused-vars */
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const Entry = require('./models/entry')
 require('dotenv').config()
 
-app = express()
+const app = express()
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token('data', (req, res) => { 
-    const data = {"name": req.body.name, "number": req.body.number};
+morgan.token('data', (req, res) => {
+    const data = { 'name': req.body.name, 'number': req.body.number }
     const jsonData = JSON.stringify(data)
     const method = JSON.stringify(req.route.methods.post)
     if(method) {
@@ -23,34 +24,27 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 app.use(cors())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        'id': 1,
+        'name': 'Arto Hellas',
+        'number': '040-123456'
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        'id': 2,
+        'name': 'Ada Lovelace',
+        'number': '39-44-5323523'
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        'id': 3,
+        'name': 'Dan Abramov',
+        'number': '12-43-234345'
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        'id': 4,
+        'name': 'Mary Poppendieck',
+        'number': '39-23-6423122'
     }
 ]
-
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(p => p.id))
-        : 0
-    return maxId + 1
-}
 
 app.get('/info', (request, response) => {
     const dataLength = persons.length
@@ -63,26 +57,26 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons', (request, response, next) => {
     Entry.find({})
-    .then(entries => {
-        response.json(entries)
-    })
-    .catch(error => next(error))
+        .then(entries => {
+            response.json(entries)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
     Entry.findById(request.params.id)
-    .then(entry => {
-        response.json(entry)
-    })
-    .catch(error => next(error))
+        .then(entry => {
+            response.json(entry)
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Entry.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -100,10 +94,10 @@ app.post('/api/persons', (request, response, next) => {
     })
 
     person.save()
-    .then(savedPerson => {
-        response.json(savedPerson)
-    })
-    .catch(error => next(error))
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -114,16 +108,16 @@ app.put('/api/persons/:id', (request, response, next) => {
     }
 
     Entry.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
-        response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-  
+
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
@@ -137,7 +131,7 @@ const errorHandler = (error, request, response, next) => {
 
     next(error)
 }
-  
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT
--- models/entry.js
+++ models/entry.js
@@ -1,28 +1,29 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
-require('dotenv').config();
+require('dotenv').config()
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    // eslint-disable-next-line no-unused-vars
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const entrySchema = mongoose.Schema({
     name: {
-      type: String,
-      minlength: 3,
-      unique: true
+        type: String,
+        minlength: 3,
+        unique: true
     },
     number: {
-      type: String,
-      minlength: 8
+        type: String,
+        minlength: 8
     }
 })
 entrySchema.plugin(uniqueValidator)
@@ -34,5 +35,5 @@ entrySchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
-      
+
 module.exports = mongoose.model('Entry', entrySchema)
